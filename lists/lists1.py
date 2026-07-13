f2c35f8fc62222b8a82b9b5afe519dff6bfdf62b
mahikothadia-coding <mahikothadia@gmail.com>
2026-07-13 17:11:05 +0100

    lists

---
+++ lists/lists1.py
@@ -0,0 +1,12 @@
+empty_list = []
+print(empty_list)
+
+numbers = [1,2,3,4,5]
+print(numbers)
+
+triples = [1,2,3,4,5] * 3
+print(triples)
+
+a_list = [100,200,300,400,500]
+a_list = a_list[::-1]
+print(a_list)
