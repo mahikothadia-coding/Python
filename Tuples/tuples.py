b93e2510475d7de6103d3965835a6cbce8747775
mahikothadia-coding <mahikothadia@gmail.com>
2026-07-20 17:11:09 +0100

    tuples

---
+++ Tuples/tuples.py
@@ -0,0 +1,14 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+
+tuplex = (4,6,2,8,3,1)
+print(tuplex)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (50,60,50,50,70)
+print(tuple1.count(50))
+
+tuplex =(2,4,6,8,10,12,14,16,18,20)
+_slice = tuplex[3:8]
